9343ba948c6a5d6a53a49c31d5efb67cbeabe80c
Alexandre Strzelewicz <strzelewicz.alexandre@gmail.com>
2017-07-31 08:52:40 -0700

    Adapt configuration for emacs 25

--- .emacs.el
+++ .emacs.el
@@ -294,7 +294,7 @@ line instead."
 (add-to-list 'auto-mode-alist '("\\.json5$" . json-mode))
 
 (add-to-list 'auto-mode-alist '("\\.ejs$" . html-mode))
-(add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
+;;(add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
 (add-hook 'js-mode-hook 'js2-minor-mode)
 (add-to-list 'interpreter-mode-alist '("node" . js2-mode))
 
